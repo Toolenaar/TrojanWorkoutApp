0be39e0c9e7707532f2735c4fb46c8004ac99a3f
euanleith <51672571+euanleith@users.noreply.github.com>
2021-02-27 20:40:15 +0000

    added workout page states and questions outlines

--- workoutapp/lib/workouts.dart
+++ workoutapp/lib/workouts.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-/*
-
- */
+//todo rests
+//todo quit
 
 class Workouts extends StatelessWidget {
   @override
@@ -11,48 +10,11 @@ class Workouts extends StatelessWidget {
     return CupertinoApp(
       routes: {
         '/': (_) => WorkoutsHomePage(),
-        'next': (_) => ExercisePage(),
+        'exercise': (_) => ExercisePage(pageIndex:0),
+        'questions': (_) => QuestionsPage(),
       },
-      // child: const Text("today's workout"),
-      // onPressed: () {workout(context);},
     );
   }
-
-  // void workout(BuildContext context) {
-  //   final q = Queue<int>();
-  //   q.add(1);
-  //   q.add(2);
-  //   q.add(3);
-  //   q.add(4);
-  //   Navigator.of(context).push(
-  //       CupertinoPageRoute<void>(
-  //         builder: (BuildContext context) {
-  //           return Row(
-  //             children: [
-  //               Align(
-  //                 alignment: Alignment.topLeft,
-  //                 child: CupertinoButton(
-  //                   child: const Text('Back'),
-  //                   onPressed: () {
-  //                     Navigator.of(context).pop();
-  //                   },
-  //                 ),
-  //               ),
-  //               Align(
-  //                 alignment: Alignment.topRight,
-  //                 child: CupertinoButton(
-  //                   child: const Text('Forward'),
-  //                   onPressed: () {
-  //                     Navigator.of(context).push();
-  //                   },
-  //                 ),
-  //               ),
-  //             ],
-  //           );
-  //         },
-  //       )
-  //   );
-  // }
 }
 
 class WorkoutsHomePage extends StatelessWidget {
@@ -60,12 +22,15 @@ class WorkoutsHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return CupertinoButton(
       child: const Text("today's workout"),
-      onPressed: () => Navigator.pushNamed(context,'next'),
+      onPressed: () => Navigator.pushNamed(context,'exercise'),
     );
   }
 }
 
 class ExercisePage extends StatelessWidget {
+  final arr = List<int>.generate(5, (i) => i);
+  int pageIndex;
+  ExercisePage({Key key, @required this.pageIndex}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -75,6 +40,7 @@ class ExercisePage extends StatelessWidget {
           child: CupertinoButton(
             child: const Text('Back'),
             onPressed: () {
+              pageIndex--;
               Navigator.of(context).pop();
             },
           ),
@@ -84,11 +50,82 @@ class ExercisePage extends StatelessWidget {
           child: CupertinoButton(
             child: const Text('Forward'),
             onPressed: () {
-              Navigator.pushNamed(context,'next');
+              pageIndex++;
+              if (pageIndex < arr.length) { // go to next exercise
+                Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => ExercisePage(pageIndex: pageIndex)));
+              } else { // go to questions
+                Navigator.pushNamed(context,'questions');
+              }
             },
           ),
         ),
+        Align(
+          alignment: Alignment.center,
+          child: CupertinoButton(
+            child: Text(arr[pageIndex].toString()),
+            onPressed: () {
+            },
+          ),
+        )
       ],
+    );
+  }
+}
+class QuestionsPage extends StatefulWidget {
+  @override
+  _QuestionsPageState createState() => _QuestionsPageState();
+}
+
+class _QuestionsPageState extends State<QuestionsPage> {
+  String v;
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+        children: [
+          Card(
+              child: ListTile(
+                  title: Text("question 1")
+              )
+          ),
+          Card(
+              child: ListTile(
+                  title: Text("answer 1"),
+                  leading: Radio(
+                      groupValue: v,
+                      value: "a1",
+                      onChanged: (value) {
+                        setState(() {
+                          v = value;
+                        });
+                      }
+                  )
+              )
+          ),
+          Card(
+              child: ListTile(
+                  title: Text("answer 2"),
+                  leading: Radio(
+                      groupValue: v,
+                      value: "a2",
+                      onChanged: (value) {
+                        setState(() {
+                          v = value;
+                        });
+                      }
+                  )
+              )
+          ),
+          CupertinoButton(
+            child: const Text("submit"),
+            onPressed: () {
+              //process v
+
+              //return home
+              Navigator.popUntil(context, ModalRoute.withName('/'));
+            }
+          )
+        ]
     );
   }
 }
