baf70bedd497f241eff41ac26529087619452a29
jaycowan <jaycowan3@gmail.com>
2021-02-28 01:16:47 +0000

    work on user auth

--- workoutapp/lib/auth/login.dart
+++ workoutapp/lib/auth/login.dart
@@ -1,21 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:workoutapp/auth/baseAuth.dart';
 
 class LoginPage extends StatefulWidget {
-  LoginPage({Key key}) : super(key: key);
+  LoginPage({this.auth, this.loginCallback, this.loginRequestName});
+
+  final BaseAuth auth;
+  final VoidCallback loginCallback;
+  final VoidCallback loginRequestName;
 
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  bool _isLoading;
+  bool _isLoginForm;
+  String _email;
+  String _password;
+  String _errorMessage;
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _errorMessage = '';
+    _isLoading = false;
+    _isLoginForm = true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-        body: PageView(
-          children: const [],
-        ),
-      ),
+          body: Stack(
+        children: <Widget>[_showForm()],
+      )),
     );
+  }
+
+  Widget _showForm() {}
+
+  Widget showProgress() {
+    if (_isLoading) {
+      return Center(child: CircularProgressIndicator());
+    }
+    return Container(
+      height: 1.0,
+      width: 5.0,
+      child: Text('error'),
+    );
+  }
+
+  void toggleFormMode() {
+    /* TODO: Implement methods below
+    resetForm();
+    setState(() {
+      _isLoginForm = !_isLoginForm;
+    });
+    */
+    throw UnimplementedError();
   }
 }
