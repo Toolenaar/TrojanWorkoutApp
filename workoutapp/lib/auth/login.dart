2ff7438159928c0c24847aecc21fd3f4ffa52308
jaycowan <jaycowan3@gmail.com>
2021-03-01 22:41:00 +0000

    signin unfinished

--- workoutapp/lib/auth/login.dart
+++ workoutapp/lib/auth/login.dart
@@ -1,5 +1,10 @@
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
 import 'package:workoutapp/auth/baseAuth.dart';
+import 'package:email_validator/email_validator.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({this.auth, this.loginCallback, this.loginRequestName});
@@ -60,4 +65,220 @@ class _LoginPageState extends State<LoginPage> {
     */
     throw UnimplementedError();
   }
+
+  Widget _showForm() {
+    final size = MediaQuery.of(context).size;
+    final width = size.width;
+    final height = size.height;
+    // TODO: Add in a logo
+    return Stack(
+      children: [
+        Container(
+          padding: EdgeInsets.all(24.0),
+          child: new Form(
+              child: Padding(
+            padding: EdgeInsets.only(top: height * .2),
+            child: new ListView(
+              shrinkWrap: true,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 30),
+                  child: Text(
+                    'Sign In',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 26, color: Colors.red),
+                  ),
+                ),
+                _googleSignInButton(),
+                _emailSignInButton()
+              ],
+            ),
+          )),
+        )
+      ],
+    );
+  }
+
+  bool validateAndSave(_key) {
+    final form = _key.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool validateEmailAndSave(_key) {
+    final form = _key.currentState;
+    if (EmailValidator.validate(_email)) {
+      form.save();
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<String> signUpUser(_email, _password, key) async {
+    setState(() {
+      _errorMessage = '';
+      _isLoading = true;
+    });
+    if (validateAndSave(key)) {
+      String userId = '';
+      try {
+        userId = await widget.auth.signUp(_email, _password, context);
+        setState(() {
+          _isLoading = false;
+        });
+        if (userId.length > 0 && userId != null && _isLoginForm) {
+          final User user = _auth.currentUser;
+          assert(user.uid == userId);
+          Navigator.pop(context);
+          return '$user';
+        }
+      } catch (e) {
+        log('///////////////////////////');
+        Toast.show(e.toString().substring(30), context,
+            duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+        print(e.toString());
+        setState(() {
+          _isLoading = false;
+          _errorMessage = e.message;
+          _formKey.currentState.reset();
+        });
+      }
+    }
+  }
+
+  Future<String> signInUser(_email, _password, key) async {
+    setState(() {
+      _errorMessage = '';
+      _isLoading = true;
+    });
+    if (validateAndSave(key)) {
+      String userId = '';
+      try {
+        userId = await widget.auth.signIn(_email, _password, context);
+        setState(() {
+          _isLoading = false;
+        });
+        if (userId.length > 0 && userId != null && _isLoginForm) {
+          final User user = _auth.currentUser;
+          assert(user.uid = userId);
+          Navigator.pop(context);
+          return '$user';
+        }
+      } catch (e) {
+        log('///////////////////////////');
+        Toast.show(e.toString().substring(30), context,
+            duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+        print(e.toString());
+        setState(() {
+          _isLoading = false;
+          _errorMessage = e.message;
+          _formKey.currentState.reset();
+        });
+      }
+    }
+  }
+
+  Future<String> resetPassword(_password, key) async {
+    setState(() {
+      _errorMessage = '';
+      _isLoading = true;
+    });
+    if (validateEmailAndSave(key)) {
+      String userId = '';
+      try {
+        await widget.auth.resetPassword(_password, context);
+        setState(() {
+          _isLoading = false;
+        });
+      } catch (e) {
+        log('///////////////////////////');
+        Toast.show(e.toString().substring(30), context,
+            duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+        print(e.toString());
+        setState(() {
+          _isLoading = false;
+          _errorMessage = e.message;
+          _formKey.currentState.reset();
+        });
+      }
+    }
+  }
+
+//------------------------Email Sign In----------------------//
+  PageController _emailController = PageController(initialPage: 0);
+
+  void _emailSignInPopup(context) {
+    final size = MediaQuery.of(context).size;
+    final width = size.width;
+    final height = size.height;
+
+    showModalBottomSheet(
+            context: context,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+            builder: (BuildContext bc) {
+              return Container(
+                height: height * .8,
+                padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: Center(
+                  child: Container(
+                    child: PageView(
+                      controller: _emailController,
+                      scrollDirection: Axis.horizontal,
+                      children: [_signUpForm(), _signInForm()],
+                    ),
+                  ),
+                ),
+              );
+            },
+            isScrollControlled: true)
+        .then((val) {
+      if (FirebaseAuth.instance.currentUser != null) {
+        widget.loginRequestName();
+      }
+    });
+  }
+
+  final _signInKey = GlobalKey<FormState>();
+
+  Widget _signInForm() {
+    return Form(
+      key: _signInKey,
+      child: Column(
+        children: [
+          Icon(Icons.mail_outline, size: 70, color: Colors.grey[700]),
+          showEmailInputSignIn(),
+          showPasswordInputSignIn(),
+          showPrimarySignInButton(),
+          showSecondarySignInButton(),
+          showForgotPasswordButton()
+        ],
+      ),
+    );
+  }
+
+  final _signUpKey = GlobalKey<FormState>();
+  Widget _signUpForm() {
+    return Form(
+      key: _signUpKey,
+      child: Column(
+        children: [
+          Icon(Icons.mail_outline, size: 70, color: Colors.grey[700]),
+          showEmailInputSignUp(),
+          showPasswordInputSignUp(),
+          showPrimarySignUpButton(),
+          showSecondarySignUpButton()
+        ],
+      ),
+    );
+  }
+
+  bool err = true;
+
+  
 }
