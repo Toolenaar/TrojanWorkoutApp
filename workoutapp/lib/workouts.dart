79eb6e2b7430be1b08af547322c5063576eb23b0
euanleith <51672571+euanleith@users.noreply.github.com>
2021-03-04 17:05:03 +0000

    added workout timer -removed back button (not sure if it's necessary and it makes stuff hard) added subscription & daily challenge buttons

--- workoutapp/lib/workouts.dart
+++ workoutapp/lib/workouts.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -12,7 +14,7 @@ class Workouts extends StatelessWidget {
     return CupertinoApp(
       routes: {
         '/': (_) => WorkoutsHomePage(exercises),
-        'exercise': (_) => ExercisePage(pageIndex:0),
+        'exercise': (_) => ExercisePage(0),
         'finishQuestions': (_) => QuestionsPage(finishQuestions),
         'quitQuestions': (_) => QuestionsPage(quitQuestions)
       },
@@ -22,6 +24,7 @@ class Workouts extends StatelessWidget {
 
 class WorkoutsHomePage extends StatelessWidget {
   final exercises;
+
   const WorkoutsHomePage(this.exercises, {
     Key key,
   }) : super(key: key);
@@ -29,21 +32,32 @@ class WorkoutsHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        CupertinoButton(
-          child: const Text("today's workout"),
-          onPressed: () => Navigator.pushNamed(context,'exercise')
-        ),
-        ListView.builder( // list of exercises in workout
-          shrinkWrap: true,
-          itemCount: exercises.length,
-          itemBuilder: (BuildContext context, int index) {
-            return ExerciseWidget(exercises[index]);
-          },
-        )
-      ]
+        children: [
+          Row(
+              children: [
+                CupertinoButton(
+                    child: const Text("Subscription"),
+                    onPressed: () => {} //do stuff
+                ),
+                CupertinoButton(
+                    child: const Text("Daily Challenge"),
+                    onPressed: () => {} //do stuff
+                ),
+              ]
+          ),
+          CupertinoButton(
+              child: const Text("Today's workout"),
+              onPressed: () => Navigator.pushNamed(context, 'exercise')
+          ),
+          ListView.builder( // list of exercises in workout
+            shrinkWrap: true,
+            itemCount: exercises.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ExerciseWidget(exercises[index]);
+            },
+          )
+        ]
     );
-
   }
 }
 
@@ -69,46 +83,54 @@ class ExerciseWidget extends StatelessWidget {
   }
 }
 
-class ExercisePage extends StatelessWidget {
-  final arr = List<int>.generate(5, (i) => i);
-  final int pageIndex;
+class ExercisePage extends StatefulWidget {
+  final int pageIndex; // exercise data
+  ExercisePage(this.pageIndex, {
+    Key key,
+  }) : super(key: key);
+  @override
+  _ExercisePageState createState() => _ExercisePageState();
+}
 
-  ExercisePage({Key key, @required this.pageIndex}) : super(key: key);
+class _ExercisePageState extends State<ExercisePage> {
+  final arr = List<int>.generate(5, (i) => i);
+
+  nextPage(context) {
+    if (widget.pageIndex+1 < arr.length) { // go to next exercise
+      Navigator.pushReplacement(context, MaterialPageRoute(
+          builder: (context) => ExercisePage(widget.pageIndex + 1)));
+    } else { // go to finish questions
+      Navigator.pushNamed(context, 'finishQuestions');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    var timer = new Timer(Duration(seconds: 2), () {nextPage(context);});
     return Row(
       children: [
+        // CupertinoButton(
+        //   child: const Text('Back'),
+        //   onPressed: () {
+        //     widget.timer.cancel();
+        //     Navigator.of(context).pop(true);
+        //   },
+        // ),
         CupertinoButton(
-          child: const Text('Back'),
+          child: const Text('Skip'),
           onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-        CupertinoButton(
-          child: const Text('Forward'),
-          onPressed: () {
-            if (pageIndex+1 < arr.length) { // go to next exercise
-              Navigator.push(context, MaterialPageRoute(
-                  builder: (context) => ExercisePage(pageIndex: pageIndex+1)));
-            } else { // go to questions
-              Navigator.pushNamed(context, 'finishQuestions');
-            }
-          },
+            timer.cancel();
+            nextPage(context);
+          }
         ),
         CupertinoButton(
           child: const Text('Quit'),
           onPressed: () {
-            Navigator.pushNamed(context, 'quitQuestions');
+            timer.cancel();
+            Navigator.pushReplacementNamed(context, 'quitQuestions');
           },
         ),
-        Align(
-          alignment: Alignment.center,
-          child: CupertinoButton(
-            child: Text(arr[pageIndex].toString()),
-            onPressed: () {},
-          ),
-        )
+        Text(arr[widget.pageIndex].toString()),
       ],
     );
   }
@@ -138,10 +160,10 @@ class _QuestionsPage extends State<QuestionsPage> {
           CupertinoButton(
             child: const Text("submit"),
             onPressed: () {
-              //process v
+              //todo how to access answers?
 
               //return home
-              Navigator.popUntil(context, ModalRoute.withName('/'));
+              Navigator.popUntil(context, ModalRoute.withName('/')); // todo go to workout summary first?
             }
           )
         ]
