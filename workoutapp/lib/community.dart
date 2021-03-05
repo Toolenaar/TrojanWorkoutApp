397065e308dfa3ebd83f3ef07f028bfe982399ea
alannahwalsh <alannahwalsh@github.com>
2021-03-05 16:39:26 +0000

    Added buttons to Community

--- workoutapp/lib/community.dart
+++ workoutapp/lib/community.dart
@@ -4,6 +4,40 @@ import 'package:flutter/material.dart';
 class Community extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blue, child: Text('Community'));
+    return CupertinoApp(
+      routes: {
+        '/': (_) => CommunityHomePage(),
+        //'principles': (_) => PrinciplesPage(),
+        //'testimonials': (_) => TestimonialsPage()
+      },
+    );
+  }
+}
+
+class CommunityHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      CupertinoButton(
+          child: Text("Welcome to Community",
+              style: TextStyle(fontSize: 20.0, color: Colors.black87)),
+          onPressed: () => {}),
+      CupertinoButton.filled(
+          child: Text("Mental Power Principles",
+              style: TextStyle(fontSize: 14.0, color: CupertinoColors.white)),
+          onPressed: () => {}),
+      CupertinoButton.filled(
+          child: Text("Upcoming Events",
+              style: TextStyle(fontSize: 14.0, color: CupertinoColors.white)),
+          onPressed: () => {}),
+      CupertinoButton.filled(
+          child: Text("Trojan Workout Website\nMartijn Bos",
+              style: TextStyle(fontSize: 14.0, color: CupertinoColors.white)),
+          onPressed: () => {}),
+      CupertinoButton.filled(
+          child: Text("Testimonials",
+              style: TextStyle(fontSize: 14.0, color: CupertinoColors.white)),
+          onPressed: () => {}),
+    ]);
   }
 }
