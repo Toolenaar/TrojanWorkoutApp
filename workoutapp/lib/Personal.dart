89b5aaead0592fab0a68bb728f7956dc790b6543
James Lunt <50912137+james-lunt@users.noreply.github.com>
2021-03-10 12:13:00 +0000

    Added record bars with dropdown
    I want the dropdowns to have editable text

--- workoutapp/lib/Personal.dart
+++ workoutapp/lib/Personal.dart
@@ -2,18 +2,118 @@ import 'package:flutter/cupertino.dart';
 //import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
+bool _isEditingText = false;
+TextEditingController _editingController;
+//String initialText = "Initial Text";
+
+/*@override
+void initState() {
+  initState();
+  _editingController = TextEditingController(text: initialText);
+}
+@override
+void dispose() {
+  _editingController.dispose();
+  dispose();
+}*/
+
+//Main class
 class Personal extends StatelessWidget {
+  final exercises = List<int>.generate(5, (i) => i); // today's exercises data
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.green,
-          ),
-          child: Text('WIP'),
-        )
-      ],
+    return CupertinoApp(
+      routes: {
+        '/': (_) => PersonalHomePage(exercises),
+      },
     );
   }
 }
+
+//Home Page
+class PersonalHomePage extends StatefulWidget {
+  final exercises;
+  const PersonalHomePage(
+    this.exercises, {
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _PersonalHomePage createState() => _PersonalHomePage();
+}
+
+class _PersonalHomePage extends State<PersonalHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      ListView.builder(
+        // list of exercises in workout
+        shrinkWrap: true,
+        itemCount: widget.exercises.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ExerciseWidget(widget.exercises[index]);
+        },
+      )
+    ]);
+  }
+}
+
+class ExerciseWidget extends StatefulWidget {
+  final int exercise; // exercise data
+  const ExerciseWidget(
+    this.exercise, {
+    Key key,
+  }) : super(key: key);
+  @override
+  _ExerciseWidget createState() => _ExerciseWidget();
+}
+
+class _ExerciseWidget extends State<ExerciseWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.only(top: 36.0, left: 6.0, right: 6.0, bottom: 6.0),
+        child: ExpansionTile(
+            title: Text('Record ' + widget.exercise.toString()),
+
+            children: <Widget>[
+              Text ('Yes')
+              //todo editable text in widget
+            ]),
+      ),
+    );
+  }
+}
+
+/*Widget _editTitleTextField(){
+  if (_isEditingText)
+    return Center(
+      child: TextField(
+        onSubmitted: (newValue){
+          setState(() {
+            initialText = newValue;
+            _isEditingText =false;
+          });
+        },
+        autofocus: true,
+        controller: _editingController,
+      ),
+    );
+  return InkWell(
+    onTap: () {
+      setState(() {
+        _isEditingText = true;
+      });
+    },
+    child: Text(
+  initialText,
+  style: TextStyle(
+    color: Colors.black,
+    fontSize: 18.0,
+  ),
+    )
+ );
+}
+*/
