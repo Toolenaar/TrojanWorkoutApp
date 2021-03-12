205a2f15746f16ae6ceb94108b78b0e8ef284086
alannahwalsh <72142210+alannahwalsh@users.noreply.github.com>
2021-03-12 13:15:16 +0000

    added principles + reviews page

--- workoutapp/lib/community.dart
+++ workoutapp/lib/community.dart
@@ -7,8 +7,8 @@ class Community extends StatelessWidget {
     return CupertinoApp(
       routes: {
         '/': (_) => CommunityHomePage(),
-        //'principles': (_) => PrinciplesPage(),
-        //'testimonials': (_) => TestimonialsPage()
+        'principles': (_) => PrinciplesPage(),
+        'reviews': (_) => ReviewsPage()
       },
     );
   }
@@ -17,27 +17,72 @@ class Community extends StatelessWidget {
 class CommunityHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-      CupertinoButton(
-          child: Text("Welcome to Community",
-              style: TextStyle(fontSize: 20.0, color: Colors.black87)),
-          onPressed: () => {}),
-      CupertinoButton.filled(
-          child: Text("Mental Power Principles",
-              style: TextStyle(fontSize: 14.0, color: CupertinoColors.white)),
-          onPressed: () => {}),
-      CupertinoButton.filled(
-          child: Text("Upcoming Events",
-              style: TextStyle(fontSize: 14.0, color: CupertinoColors.white)),
-          onPressed: () => {}),
-      CupertinoButton.filled(
-          child: Text("Trojan Workout Website\nMartijn Bos",
-              style: TextStyle(fontSize: 14.0, color: CupertinoColors.white)),
-          onPressed: () => {}),
-      CupertinoButton.filled(
-          child: Text("Testimonials",
-              style: TextStyle(fontSize: 14.0, color: CupertinoColors.white)),
-          onPressed: () => {}),
-    ]);
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: const Text('Welcome to Community!'),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            CupertinoButton.filled(
+                child: Text("Mental Power Principles",
+                    style: TextStyle(
+                        fontSize: 14.0, color: CupertinoColors.white)),
+                onPressed: () => {
+                      Navigator.of(context).pushNamed("principles")
+                    }), // goes to new page displaying power principles
+            CupertinoButton.filled(
+                child: Text("Upcoming Events",
+                    style: TextStyle(
+                        fontSize: 14.0, color: CupertinoColors.white)),
+                onPressed: () =>
+                    {}), // go to events on the external Trojan site
+            CupertinoButton.filled(
+                child: Text("Trojan Workout Website \nMartijn Bos",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 14.0, color: CupertinoColors.white)),
+                onPressed: () =>
+                    {}), // go to homepage on the external Trojan site
+            CupertinoButton.filled(
+                child: Text("Reviews",
+                    style: TextStyle(
+                        fontSize: 14.0, color: CupertinoColors.white)),
+                onPressed: () => {
+                      Navigator.of(context).pushNamed("reviews")
+                    }), // goes to new page with list of reviews
+          ],
+        ));
+  }
+}
+
+class PrinciplesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: const Text("Mental Power Principles"),
+      ),
+      child: CupertinoButton(
+        child: const Text(""),
+        onPressed: () {},
+      ),
+    );
+  }
+}
+
+class ReviewsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text("Reviews"),
+      ),
+      child: CupertinoButton(
+        child: const Text(""),
+        onPressed: () {},
+      ),
+    );
   }
 }
