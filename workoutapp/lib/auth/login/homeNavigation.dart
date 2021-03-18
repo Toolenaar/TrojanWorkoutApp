9c619044c020776bd4338fbdc9494371310bc878
jaycowan <jaycowan3@gmail.com>
2021-03-18 14:08:36 +0000

    started home nav

---
+++ workoutapp/lib/auth/login/homeNavigation.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeNavigation extends StatefulWidget {
+  HomeNavigation({Key key}) : super(key: key);
+
+  @override
+  _HomeNavigationState createState() => _HomeNavigationState();
+}
+
+class _HomeNavigationState extends State<HomeNavigation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: child,
+    );
+  }
+}
---
+++ workoutapp/lib/auth/login/homePage.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:workoutapp/auth/baseAuth.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.auth, this.userId, this.logoutCallback})
+      : super(key: key);
+
+  final BaseAuth auth;
+  final VoidCallback logoutCallback;
+  final String userId;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final width = size.width;
+    final height = size.height;
+    
+    return Container(
+      child: child,
+    );
+  }
+}
